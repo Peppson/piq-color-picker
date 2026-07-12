7ac7eec816d7518fdf0cc65acca984190e69c89a
Jesper <jw_jw@live.com>
2026-07-12 14:22:26 +0200

    app name = Piq + started to test store images

--- ColorPicker/Settings/Config.cs
+++ ColorPicker/Settings/Config.cs
@@ -18,14 +18,15 @@ public static class Config
     public const bool Log_UpdateZoomView_Frametimes = false;
     public const bool Log_UpdateZoomView_FunctionCallRate = false;
 
-    // Constants
-    public const string AppName = "TODO";
-    public const string GithubLink = "https://github.com/Peppson/color-grab"; // todo
+    // Constants 
+    public const string AppName = "Piq";
+    public const string AppNameFull = $"{AppName} - Color Picker";
+    public const string GithubLink = "https://github.com/Peppson/piq-color-picker";
     public const int InitialZoomLevel = 19;
     public const double MinZoomLevel = 11; // Uneven needed for px centering
     public const double MaxZoomLevel = 91;
     public const int StatusMessageDuration_ms = 2000;
     public static readonly string RawVersionNumber =
         Assembly.GetExecutingAssembly().GetName().Version!.ToString(3) ?? throw new InvalidOperationException("Failed to get version number");
-    public static readonly string VersionNumber = "v" + RawVersionNumber;
+    public static readonly string VersionNumber = /* "v" +  */RawVersionNumber;
 }
